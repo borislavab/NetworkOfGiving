e2b2726c05a2bc7eef7e8ecd0d0834281c8dfdb1
bbagaliyska <bbagaliyska@gmail.com>
2020-05-23 00:18:11 +0300

    Configure endpoint authorization
    The /users/login and /users/register will be the only endpoints
    reachable from anyone. All other endpoints require an authenticated
    user.

--- server/network-of-giving/src/main/java/com/example/networkofgiving/controllers/UsersController.java
+++ server/network-of-giving/src/main/java/com/example/networkofgiving/controllers/UsersController.java
@@ -1,13 +1,26 @@
 package com.example.networkofgiving.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UsersController {
 
-    @GetMapping("/")
-    public String hi() {
-        return "Hello Spring!";
+    @PostMapping("/login")
+    public String login() {
+        return "Logged in!";
+    }
+
+    @PostMapping("/register")
+    public String register() {
+        return "Registered!";
+    }
+
+    @GetMapping("/authenticated")
+    public String authenticated() {
+        return "Authenticated!";
     }
 }
