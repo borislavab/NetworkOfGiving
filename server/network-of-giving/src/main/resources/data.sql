4770bfb4e4d431ea67214ec682f7f5717ef61ac3
bbagaliyska <bbagaliyska@gmail.com>
2020-05-23 13:07:28 +0300

    Create a users table and seed hardcoded user
    Created a basic user entity class and configured Spring to automatically
    create a table "users" for the entity if it doesn't exist. Seed a
    hardcoded user via data.sql script

---
+++ server/network-of-giving/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users(username, password)
+values('username', '$2a$10$vmSIdhkdgbqvIM8ZVMttzOIEDZgR/ibw51a7NE.KR34c04Zf/iDYq');
+
+-- second argument is the BCrypt encoding of the string "password"
